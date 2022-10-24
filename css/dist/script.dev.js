106eb97f5cea1c9bab97f22868ac633c3d043932
Arakazagraziella <graziella.arakaza@gmail.com>
2022-10-24 18:45:27 +0200

    my changes

--- css/dist/script.dev.js
+++ css/dist/script.dev.js
@@ -2,18 +2,18 @@
 
 var header = document.querySelector("header");
 window.addEventListener("scroll", function () {
-  header.classList.Toggle("sticky", window.scrollY > 0);
+  header.classList.toggle("sticky", window.scrollY > 0);
 });
-var menu = document.querySelector('#menu-icon');
-var navbar = document.querySelector('.navbar');
+var menu = document.querySelector('menu-icon');
+var navbar = document.querySelector('navbar');
 
 menu.onclick = function () {
   menu.classList.toggle('bx-x');
-  navbar.classList.toggle('active');
+  menu.classList.toggle('active');
 };
 
 window.onscroll = function () {
   menu.classList.remove('bx-x');
   navbar.classList.remove('active');
 };
-//# sourceMappingURL=Script.dev.js.map
+//# sourceMappingURL=script.dev.js.map
